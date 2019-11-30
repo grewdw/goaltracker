4cb6733dd7644e599b53ac52891747c5ce2575da
David G <davidgrew@hotmail.co.uk>
2019-11-30 10:30:05 +0000

    update migration script and server port

--- persistence/src/main/resources/migration/V1_createschema.sql
+++ persistence/src/main/resources/migration/V1_createschema.sql
@@ -1,32 +1,36 @@
 CREATE SCHEMA goaltracker;
 
+USE goaltracker;
+
 CREATE TABLE activity (
-  id bigint(20),
-  name varchar(255));
+  id bigint(20) auto_increment,
+  name varchar(255),
+  primary key (id));
 
 CREATE TABLE activity_record (
-  id bigint(20),
+  id bigint(20) auto_increment,
   end datetime,
   start datetime,
-  activity_id bigint(20));
+  activity_id bigint(20),
+  primary key (id));
 
 CREATE TABLE task(
-  id bigint(20),
-  name varchar(255));
+  id bigint(20) auto_increment,
+  name varchar(255),
+  primary key (id));
 
 CREATE TABLE task_record(
-  id bigint(20),
+  id bigint(20) auto_increment,
   count int(11),
   time datetime,
-  task_id bigint(20));
+  task_id bigint(20),
+  primary key (id));
 
 CREATE TABLE task_target(
-  id bigint(20),
-  count int(11)
-  current bit(1)
-  direction int(11)
-  period int(11)
-  task_id bigint(20));
-
-CREATE TABLE hibernate_sequence(
-  next_val bigint(20))
+  id bigint(20) auto_increment,
+  count int(11),
+  current bit(1),
+  direction int(11),
+  period int(11),
+  task_id bigint(20),
+  primary key (id));
